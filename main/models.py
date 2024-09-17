41af55d4e320cd41fd7a80410615f3070b4bce35
SalomoImmanuel <https://github.com/SalomoManullang/Goodsify.git>
2024-09-17 19:04:25 +0700

    dsnsa

--- main/models.py
+++ main/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+import uuid  
+
 
 class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # tambahkan baris ini
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
--- main/views.py
+++ main/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect   # Tambahkan import redirect di baris ini
+from main.forms import ProductForm
+from main.models import Product
 
 
 def show_main(request):
+    Products = Product.objects.all()
     context = {
         'name' : 'Teh Botol Sosro',
         'price': 'Rp.20.000',
         'description': 'Nikmati kesegaran yang tiada duanya dengan Teh Botol Sosro, minuman teh melati berkualitas tinggi yang dikemas praktis dan siap diminum kapan saja. Terbuat dari daun teh pilihan dan bunga melati alami, Teh Botol Sosro menghadirkan rasa manis yang pas dan aroma teh yang khas, memberikan kesegaran sempurna di setiap tegukan.',
         'rating': '3',
+        'Product' : Products
     }
 
     return render(request, "main.html", context)
+
+def create_product(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_product.html", context)
+
