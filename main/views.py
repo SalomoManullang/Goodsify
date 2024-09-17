8b21315fddba4265f504d1a0a7733b3efa213905
SalomoImmanuel <https://github.com/SalomoManullang/Goodsify.git>
2024-09-17 20:21:59 +0700

    da

--- main/views.py
+++ main/views.py
@@ -5,13 +5,13 @@ from main.models import Product
 
 
 def show_main(request):
-    Products = Product.objects.all()
+    products = Product.objects.all()
     context = {
         'name' : 'Teh Botol Sosro',
         'price': 'Rp.20.000',
         'description': 'Nikmati kesegaran yang tiada duanya dengan Teh Botol Sosro, minuman teh melati berkualitas tinggi yang dikemas praktis dan siap diminum kapan saja. Terbuat dari daun teh pilihan dan bunga melati alami, Teh Botol Sosro menghadirkan rasa manis yang pas dan aroma teh yang khas, memberikan kesegaran sempurna di setiap tegukan.',
         'rating': '3',
-        'Product' : Products
+        'product' : products
     }
 
     return render(request, "main.html", context)
