10a7fdcec147c98de9df9060ed6ff05b6647529a
SalomoImmanuel <https://github.com/SalomoManullang/Goodsify.git>
2024-09-17 20:57:02 +0700

    sasa

--- main/views.py
+++ main/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect   # Tambahkan import redirect di baris ini
 from main.forms import ProductForm
 from main.models import Product
-
+from django.http import HttpResponse
+from django.core import serializers
 
 def show_main(request):
     products = Product.objects.all()
@@ -21,4 +22,22 @@ def create_product(request):
 
     context = {'form': form}
     return render(request, "create_product.html", context)
+
+def show_xml(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+
 
