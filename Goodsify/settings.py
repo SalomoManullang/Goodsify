4c55ba1066b688a3bb484000ab0c58e9effed40c
SalomoImmanuel <https://github.com/SalomoManullang/Goodsify.git>
2024-09-25 13:30:45 +0700

    tes

--- Goodsify/settings.py
+++ Goodsify/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://salomo-immanuel-Goodsify.pbp.cs.ui.ac.id", "https://salomo-immanuel-Goodsify.pbp.cs.ui.ac.id"]
+
+
